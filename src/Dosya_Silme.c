fbf84980e8959fe5d5c4b67d8f72a7d8fa907133
Kaan KELEŞ <kaankel40111@gmail.com>
2026-02-14 18:12:26 +0300

    Add file deletion feature in Dosya_Silme.c
    Implement file deletion functionality in Dosya_Silme.c

---
+++ src/Dosya_Silme.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define D_Listesi system("dir")
+
+#else
+
+#include <unistd.h>
+#define D_Listesi system("ls")
+
+#endif
+
+void silme(){
+    char silinen[100];
+    //system("color a");
+    system("date /T && time /t");
+    system("color b");
+    D_Listesi;
+    printf("silmek icin dosya sec\n");
+    scanf("%99s",silinen);
+    remove(silinen);
+}
